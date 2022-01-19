d15c55e22ab99e0f3f28f51531aea09d57277f6a
lihaochen <lihaochen@zhaodongdb.com>
2022-01-19 14:16:29 +0800

    [feat]first commit

---
+++ app/src/main/java/com/marco/imagecompress/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.marco.imagecompress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.marco.imagecompress.databinding.ActivityMainBinding
+import com.zhaodong.compress.ImageCompress
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val compress = ImageCompress()
+        binding.sampleText.text = compress.stringFromJNI();
+    }
+}
