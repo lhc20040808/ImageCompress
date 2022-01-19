46d3f96640d5aaa9533553dcae97d1823e2f1e35
lihaochen <lihaochen@zhaodongdb.com>
2022-01-19 14:53:19 +0800

    [feat]cmake

--- compress/src/main/cpp/compress.h
+++ compress/src/main/cpp/compress.h
@@ -1,27 +1,8 @@
+#ifndef COMPRESS_ZHAODONG_JPEG
+#define COMPRESS_ZHAODONG_JPEG
 
-#ifndef COMPRESS_COMPRESS_H_H
-#define COMPRESS_COMPRESS_H_H
 
-#include "lang.h"
-#include <stdlib.h>
-#include <android/bitmap.h>
 
-#include <setjmp.h>
-#include <jpeglib.h>
+void getStr(char* str);
 
-#define true 1
-#define false 0
-
-//#ifdef __cplusplus
-//extern "C" {
-//#endif
-JNIEXPORT jint JNICALL
-Java_com_lishuaihua_compress_ImageCompress_nativeCompressBitmap(JNIEnv *env, jclass type,
-                                                               jobject bitmap, jint quality,
-                                                               jstring dstFile_,
-                                                               jboolean optimize);
-//
-//#ifdef __cplusplus
-//}
-//#endif
-#endif //COMPRESS_COMPRESS_H_H
+#endif //COMPRESS_ZHAODONG_JPEG
